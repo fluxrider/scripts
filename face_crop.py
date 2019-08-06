49ee777b0772fc708c17654c0e23f12c63ad71d5
fluxrider <doompilot@gmail.com>
2019-08-06 13:51:30 -0400

    margin

--- face_crop.py
+++ face_crop.py
@@ -5,7 +5,7 @@ for f in /pics/*.jpg; do
   name="${filename%.*}"
   ext="${filename##*.}"
   echo "$filename..."
-  python face_crop.py "$f" "out/$name.{}.$ext"
+  python face_crop.py "$f" "out/$name.{}.$ext" .3
 done
 '''
 
@@ -16,6 +16,7 @@ import cv2
 # args
 file_in = sys.argv[1]
 file_out_pattern = sys.argv[2] # e.g. 'out{}.png'
+margin = float(sys.argv[3]) # e.g. .3
 
 # load image
 img = cv2.imread(file_in)
@@ -27,4 +28,16 @@ faces = classifier.detectMultiScale(img)
 # save them all
 for i in range(len(faces)):
   (x,y,w,h) = faces[i]
+  # typical results crop the face, but I want hair and neck to show
+  H = img.shape[0]
+  W = img.shape[1]
+  x -= int(w * margin)
+  y -= int(h * margin)
+  w += 2 * int(w * margin)
+  h += 2 * int(h * margin)
+  if x < 0: w += x; x = 0
+  if y < 0: h += y; y = 0
+  if x + w > W: w -= (x + w) - W
+  if y + h > H: h -= (y + h) - H
+  # save
   cv2.imwrite(file_out_pattern.format(i), img[y:y+h, x:x+w])
