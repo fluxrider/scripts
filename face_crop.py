ab7769ca087b194b2e16ea7ecf6c9c4d257cd42b
fluxrider <doompilot@gmail.com>
2019-08-06 13:11:53 -0400

    face crop tool

---
+++ face_crop.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import cv2
+
+# args
+file_in = sys.argv[1]
+file_out_pattern = sys.argv[2] # e.g. 'out{}.png'
+
+# load image
+img = cv2.imread(file_in)
+
+# find the faces
+classifier = cv2.CascadeClassifier('/usr/share/opencv4/haarcascades/haarcascade_frontalface_alt.xml')
+faces = classifier.detectMultiScale(img)
+
+# save them all
+for i in range(len(faces)):
+  (x,y,w,h) = faces[i]
+  cv2.imwrite(file_out_pattern.format(i), img[y:y+h, x:x+w])
